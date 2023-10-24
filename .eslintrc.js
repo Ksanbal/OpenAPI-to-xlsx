c96b5f57bfc6296141b49df19afc4f5a0e2af0fa
Ksanbal <dev.ksanbal@gmail.com>
2023-10-25 02:07:46 +0900

    Fix: eslint, prettier 충돌 삭제

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,21 +1,24 @@
 module.exports = {
+  root: true,
   parser: '@typescript-eslint/parser',
   parserOptions: {
     project: 'tsconfig.json',
     tsconfigRootDir: __dirname,
     sourceType: 'module',
   },
-  plugins: ['@typescript-eslint/eslint-plugin'],
+  plugins: ['@typescript-eslint'],
   extends: [
+    'eslint:recommended',
     'plugin:@typescript-eslint/recommended',
-    'plugin:prettier/recommended',
+    'prettier',
   ],
-  root: true,
   env: {
     node: true,
   },
   ignorePatterns: ['.eslintrc.js'],
   rules: {
+    semi: 'error',
+    '@typescript-eslint/no-unused-vars': 'error',
     '@typescript-eslint/interface-name-prefix': 'off',
     '@typescript-eslint/explicit-function-return-type': 'off',
     '@typescript-eslint/explicit-module-boundary-types': 'off',
